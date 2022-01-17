2483cc194a5c42db32e423e0a36d8324f130faec
MichaelTham10 <mikeltham01@gmail.com>
2022-01-18 00:14:21 +0700

    Final Commit 1

--- resources/views/category/category.blade.php
+++ resources/views/category/category.blade.php
@@ -11,30 +11,10 @@
 @section('content')
     <div class="container">
         <p class="text-center wow fadeInDown" id="title">Kategori</p>
-        <form action="">
-            <div class="d-flex align-items-center" id="search-bar">
-                <input type="text" id="search" class="form-control">
-                <img src="{{asset('assets/icon/search.png')}}" alt="">
-            </div>
-        </form>  
+       
     </div>
     <div class="pt-5">
         @include('components.category-grid', ['categories' => $categories])
     </div>
 @endsection
 
-@section('script')
-    <script type="text/javascript">
-        $(document).ready(function() {
-
-            $('#search-bar').keydown(function(event) {
-            
-            if (event.keyCode == 13) {
-                    this.form.submit();
-                    return false;
-                }
-            });
-
-        });
-    </script>
-@endsection
